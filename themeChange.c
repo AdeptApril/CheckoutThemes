3122aaeeb6d4bbfa1d9bf7d45aa0ae3d46de1ee6
April Monoceros <10776123+AdeptApril@users.noreply.github.com>
2015-06-05 16:34:47 -0500

    Made setInstallName() work as intended
    Other minor changes, too

--- themeChange.c
+++ themeChange.c
@@ -2,6 +2,7 @@
 #include <curl/curl.h>
 #include <curl/easy.h>
 #include <string.h>
+#define MAX_LIBRARY_NAME 20
 
 size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
     size_t written;
@@ -52,15 +53,64 @@ int extractTheme(const char * filename){
   return 0;
 }
 
+void setInstallName(char * installName, char * themeLocation) {
+    //Maybe use a loop to read characters until "Install" is seen
+    //Then take everything from there until the '.', adding "Install" to the
+    //string before adding those characters
+    int i;
+    strcpy(installName, "Install");
+    printf("installName before: %s\n", installName);
+    //printf("themeLocation[0] %c\n", themeLocation[0]);
+    if(themeLocation) { //Not sure if it's strictly necessary to check this.
+        for(i=0; i<strlen(themeLocation); i++) {
+            //printf("%c", themeLocation[i]);
+            if ( (themeLocation[i] == EOF) || (themeLocation[i] == '\n') )
+                break;
+            if (themeLocation[i] != 'I')
+                continue;
+            //Massive if statement to see if it's "Install"
+            if ( (themeLocation[i+1] == 'n') && (themeLocation[i+2] == 's') && (themeLocation[i+3] == 't') && (themeLocation[i+4] == 'a') && (themeLocation[i+5] == 'l') && (themeLocation[i+6] == 'l') ) {
+                //printf("\nthemeLocation[i] %c\n", themeLocation[i]);
+                int j;
+                j = 7;
+                //printf("themeLocation[i+j] (before while): %c\n", themeLocation[i+j]);
+                //While not ".exe"
+                while ( !( (themeLocation[i+j] == '.') && (themeLocation[i+j+1] == 'e') && (themeLocation[i+j+2] == 'x') && (themeLocation[i+j+3] == 'e') ) ){
+                    //printf("themeLocation[i+j]: %c\n", themeLocation[i+j]);
+                    //printf("themeLocation[.exe] area: %c%c%c%c\n", themeLocation[i+j], themeLocation[i+j+1], themeLocation[i+j+2], themeLocation[i+j+3] );
+                    if ( (themeLocation[i+j] == '\n') || (themeLocation[i+j] == EOF) )
+                        exit(1); //This shouldn't happen.
+                    installName[j] = themeLocation[i+j];
+                    j++;
+                    //if (j>50) //failsafe on the while loop
+                    //    break;
+                }
+            } else { //Should be reached when it's 'I' but not "Install"
+                continue;
+            }
+        }
+    }
+    printf("installName after: %s", installName);
+    return;
+}
+
 int main(void) {
     //Setup CURL and the local file
     CURL *curl;
     FILE *fp;
     char themeLocation[FILENAME_MAX];
+    char libraryName[MAX_LIBRARY_NAME];
+    short languageNum;
+    char installName[FILENAME_MAX];
 
     //Set the appropriate URL. This should be set by command line
     //The general idea is to download the appropriate text file that says
     //which theme to install.
+    //The name of the library should also be set with the command line.
+    //The language # should also be set.
+    //And the self-check # should be set.
+    strcpy(libraryName, "Sequoya");
+    languageNum = 3;
     char *url = "http://www.checkoutthemes.com/sequoya/checkout3/lang3choice.txt";
     char outfilename[FILENAME_MAX] = "themechoice.txt";
     curl = curl_easy_init();
@@ -99,6 +149,7 @@ int main(void) {
     printf("File to download: %s\n", themeLocation); //Debugging.
 
     //Download the file specified in the txt file.
+//if(0) //Used to decrease the amount I'm downloading while testing
     curl = curl_easy_init();
     if (curl) {
         struct FtpFile ftpfile = {
@@ -122,17 +173,15 @@ int main(void) {
 
         //Decompress file
         extractTheme(ftpfile.filename);
-
+//#endif
         //Run appropriate .bat file
-        //Maybe use a while loop to read characters until "Install" is seen
-        //Then take everything from there until the '.', adding "Install" to the
-        //string before adding those characters
         //This should allow changing into the install directory, and with
         //"AsLanguage3" or something added it should match the batch file.
+        setInstallName(&installName, &themeLocation);
 
         //Cleanup
         curl_easy_cleanup(curl);
         fclose(fp);
-    }
+    } //Needs to be commented out with the #endif
     return 0;
 }
