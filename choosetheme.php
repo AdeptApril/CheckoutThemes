11e7218e0609f136b99b1884c6cc543ac8d9c62a
April Monoceros <10776123+AdeptApril@users.noreply.github.com>
2015-06-09 16:42:05 -0500

    Seems to work now

--- choosetheme.php
+++ choosetheme.php
@@ -6,34 +6,12 @@
 	</script>
 	</head>
 	<body>
-		<form id="themechoice" method="POST">
-            <label for="library">Library</label>
-			<select id="library">
-				<option>Sequoya</option>
-			</select><br>
-            <label for="machineNum">Machine Number</label>
-			<select id="machineNum">
-				<option>1</option>
-				<option>2</option>
-				<option>3</option>
-				<option>4</option>
-				<option>5</option>
-				<option>6</option>
-			</select><br>
-            <label for="languageNum">Language Number</label>
-			<select id="languageNum">
-				<option>3</option>
-				<option>4</option>
-			</select><br>
-            <label for="theme">Theme</label>
-			<select id="theme">
-                <option>Storybook Clay</option>
-				<option>Storybook Nicky</option>
-			</select><br>
-			<input type="button" onclick="submit()" value="Submit">
-			<input type="button" onclick="reset()" value="Reset"><br>
-		</form>
         <?php // Echo out the current contents of the theme choices
+            $machineNum = $_POST['machineNum'];
+            $languageNum = $_POST['languageNum'];
+            $theme = $_POST['theme'];
+            $filename = "../sequoya/checkout".$machineNum."/lang".$languageNum."choice.txt";
+            file_put_contents($filename, $theme);
             Echo "The following are the current language choices for each machine at Sequoya: <br>";
             Echo "Checkout 1, language 3: ";
             echo file_get_contents( "http://www.checkoutthemes.com/sequoya/checkout1/lang3choice.txt" );
@@ -71,8 +49,8 @@
         }
         document.themechoice.onsubmit() {
         <?php
-        //$filename = "checkoutthemes.com/sequoya/checkout".machineNum."/lang".languageNum."choice.txt";
-        //file_put_contents($filename, "http://www.checkoutthemes.com/sequoya/InstallStorybookClayThemeSequoya.exe");
+        $filename = "checkoutthemes.com/sequoya/checkout".machineNum."/lang".languageNum."choice.txt";
+        file_put_contents($filename, "http://www.checkoutthemes.com/sequoya/InstallStorybookTestThemeSequoya.exe");
         echo "test";
         ?>
 		}
